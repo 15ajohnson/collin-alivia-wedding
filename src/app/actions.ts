48bbbf8c7caf0dc2574f528386d29a2f7ee9fe4c
collinmurd <collinmurdock22@gmail.com>
2025-03-23 09:43:24 -0400

    write to a google sheet. using googles library instead

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,33 @@
 "use server"
 
-import { getToken } from "@/lib/auth";
+import { google } from "googleapis";
+
+const auth = new google.auth.GoogleAuth({
+    keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS!,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
 
 export async function rsvp(rsvp: FormData) {
     console.log("RSVP received:", rsvp);
 
-    const accessToken = await getToken();
-    console.log("Access token:", accessToken);
+    const sheets = google.sheets({ version: "v4", auth });
+    const spreadsheetId = process.env.SHEET_ID!;
+    const range = "Sheet1!A:B";
+    const valueInputOption = "USER_ENTERED";
+
+    const resource = {
+        values: [[rsvp.get('name'), rsvp.get('plus-one')]]
+    };
+
+    try {
+        const result = await sheets.spreadsheets.values.append({
+            spreadsheetId,
+            range,
+            valueInputOption,
+            requestBody: resource,
+        });
+        console.log("Row added:", result.data.updates?.updatedRange);
+    } catch (error) {
+        console.error("Error adding row:", error);
+    }
 }
