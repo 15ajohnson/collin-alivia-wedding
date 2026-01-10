b039c419932693fdc8a364d6a619e73020ad1ad9
collinmurd <collinmurdock22@gmail.com>
2026-01-10 09:54:30 -0500

    some progress

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -1,15 +1,101 @@
 
 import { Game, Scene } from "phaser";
 
+const physicsBody = (gameObject: Phaser.GameObjects.GameObject) => {
+    if (gameObject.body) {
+        return gameObject.body as Phaser.Physics.Arcade.Body;
+    } else {
+        throw new Error("GameObject does not have a body");
+    }
+}
+
 class MyScene extends Scene {
+    cursorKeys!: Phaser.Types.Input.Keyboard.CursorKeys;
+    player!: Phaser.GameObjects.Triangle;
+    asteroids!: Phaser.Physics.Arcade.Group;
+
     constructor() {
         super({ key: 'MyScene' });
     }
 
+    // asset load
     preload() { }
 
+    // scene setup
     create() {
-        this.add.text(100, 100, 'Hello Phaser!', { font: '32px Arial' });
+        // input setup
+        this.cursorKeys = this.input.keyboard!.createCursorKeys();
+
+        // player setup
+        // this.player = this.add.ellipse(400, 300, 50, 50, 0xff0000)
+        this.player = this.add.triangle(400, 300, 0, 0, 40, 15, 0, 30, 0x0000ff);
+        this.physics.add.existing(this.player);
+        physicsBody(this.player).setMaxSpeed(400);
+
+        // asteroids
+        this.asteroids = this.physics.add.group();
+        for (let i = 0; i < 3; i++) {
+            const x = Phaser.Math.Between(0, this.sys.canvas.width);
+            const y = Phaser.Math.Between(0, this.sys.canvas.height);
+            const asteroid = this.add.ellipse(x, y, 50, 50, 0x00ff00);
+            this.asteroids.add(asteroid);
+
+            physicsBody(asteroid).setVelocity(Phaser.Math.Between(-100, 100), Phaser.Math.Between(-100, 100));
+        }
+    }
+
+    // game loop
+    update() {
+        this.move();
+
+        // when moving off the world bounds, wrap around
+        this.wrapBounds(this.player);
+        this.asteroids.children.iterate((asteroid) => {
+            this.wrapBounds(asteroid as Phaser.GameObjects.Shape);
+            return true;
+        })
+    }
+
+    private move() {
+        const body = physicsBody(this.player);
+        const increment = 5;
+
+        if (this.cursorKeys.up.isDown) {
+            // body.setVelocityY(body.velocity.y - increment);
+            // body.setAccelerationY()
+            // body
+            // this.player.setRotation()
+            body.setVelocity(
+                Math.cos(this.player.rotation) * body.velocity.add
+                    Math.sin(this.player.rotation) * 
+            )
+            console.log(
+                Math.cos(this.player.rotation) * body.velocity.x + increment * Math.cos(this.player.rotation),
+                Math.sin(this.player.rotation) * body.velocity.y + increment * Math.sin(this.player.rotation)
+            );
+        }
+
+        const rotationDelta = 0.04
+        if (this.cursorKeys.right.isDown) {
+            this.player.setRotation(this.player.rotation + rotationDelta);
+        } else if (this.cursorKeys.left.isDown) {
+            this.player.setRotation(this.player.rotation - rotationDelta);
+        }
+
+    }
+
+    private wrapBounds(object: Phaser.GameObjects.Shape) {
+        if (object.x < 0) {
+            object.setX(this.sys.canvas.width);
+        } else if (object.x > this.sys.canvas.width) {
+            object.setX(0);
+        }
+        if (object.y < 0) {
+            object.setY(this.sys.canvas.height);
+        } else if (object.y > this.sys.canvas.height) {
+            object.setY(0);
+        }
+
     }
 }
 
@@ -17,6 +103,9 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+    },
     scene: [
         MyScene
     ]
