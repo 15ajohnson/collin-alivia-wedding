1994b8c7328579436adfe3bad6ff2245a499be0f
collinmurd <collinmurdock22@gmail.com>
2026-06-14 17:13:31 -0400

    enable WAL on sqlite db

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,4 +7,13 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
 
+// set write-ahead logging mode
+prisma.$executeRaw`PRAGMA journal_mode = WAL;`
+  .then(() => {
+    console.log("Database connection established and journal mode set to WAL.");
+  })
+  .catch((error) => {
+    console.error("Error setting journal mode to WAL:", error);
+  });
+
 export { prisma };
